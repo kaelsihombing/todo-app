44e52338e59012a99d9c18456e5433189a87c2d7
michaelsihombing <sntmcl1@gmail.com>
2020-02-12 10:23:10 +0700

    [MIKE]Complete the User Unit Testing

--- src/tests/userTest.js
+++ src/tests/userTest.js
@@ -18,7 +18,7 @@ const user = {
     password_confirmation: '123456'
 }
 
-describe('User API', () => {
+describe('~USER API UNIT TESTING~', () => {
 
     before(done => {
         User.create({
@@ -45,6 +45,9 @@ describe('User API', () => {
                 .send(JSON.stringify(data))
                 .end(function (err, res) {
                     expect(res.status).to.equal(201);
+                    expect(res.body).to.be.an('object')
+                    expect(res.body).to.have.property('success');
+                    expect(res.body).to.have.property('data');
                     let { success, data } = res.body;
                     expect(success).to.eq(true);
                     expect(data).to.be.an('object');
@@ -69,6 +72,9 @@ describe('User API', () => {
                 .send(JSON.stringify(data))
                 .end(function (err, res) {
                     expect(res.status).to.eq(422);
+                    expect(res.body).to.be.an('object')
+                    expect(res.body).to.have.property('success');
+                    expect(res.body).to.have.property('error');
                     let { success, error } = res.body;
                     expect(success).to.eq(false);
                     expect(error).to.be.an('string');
@@ -77,7 +83,7 @@ describe('User API', () => {
                 })
         })
 
-        it('Should not create a new user due to duplication', done => {
+        it('Should not create a new user because duplication data', done => {
             let data = {
                 email: 'test01@mail.com',
                 password: '123456',
@@ -91,11 +97,37 @@ describe('User API', () => {
                 .end(function (err, res) {
                     // console.log(res.body);
                     expect(res.status).to.eq(422);
-
+                    expect(res.body).to.be.an('object')
+                    expect(res.body).to.have.property('success');
+                    expect(res.body).to.have.property('error');
                     let { success, error } = res.body;
                     expect(success).to.eq(false);
                     expect(error).to.be.an('object');
                     expect(error.errmsg).to.eq('E11000 duplicate key error collection: awesome-project_test.users index: email_1 dup key: { : "test01@mail.com" }')
+                    done();
+                })
+        })
+
+        it('Should not create a new user due to validation error', done => {
+            let data = {
+                password: '123456',
+                password_confirmation: '123456'
+            }
+
+            chai.request(server)
+                .post('/api/v1/users/register')
+                .set('Content-Type', 'application/json')
+                .send(JSON.stringify(data))
+                .end(function (err, res) {
+                    // console.log(res.body);
+                    expect(res.status).to.eq(422);
+                    expect(res.body).to.be.an('object')
+                    expect(res.body).to.have.property('success');
+                    expect(res.body).to.have.property('error');
+                    let { success, error } = res.body;
+                    expect(success).to.eq(false);
+                    expect(error).to.be.an('object');
+                    expect(error.message).to.eq('Validation failed: email: Path `email` is required.')
                     done();
                 })
         })
@@ -108,14 +140,67 @@ describe('User API', () => {
                 .set('Content-Type', 'application/json')
                 .send(JSON.stringify(user))
                 .end(function (err, res) {
-                    console.log(res.body);
+                    // console.log(res.body);
                     expect(res.status).to.eq(200)
+                    expect(res.body).to.be.an('object')
+                    expect(res.body).to.have.property('success');
+                    expect(res.body).to.have.property('data');
                     let { success, data } = res.body;
                     expect(success).to.eq(true);
                     expect(data).to.be.an('object')
                     expect(data).to.have.property('id')
                     expect(data).to.have.property('email')
                     expect(data).to.have.property('token')
+                    // expect(data.token).to.eq(decoded)
+                    done();
+                })
+        })
+
+        it('Should not successfully logged in because the password is wrong', done => {
+            let data = {
+                email: 'test01@mail.com',
+                password: 'test123',
+            }
+            chai.request(server)
+                .post('/api/v1/users/login')
+                .set('Content-Type', 'application/json')
+                .send(JSON.stringify(data))
+                .end(function (err, res) {
+                    // console.log(res.body);
+                    expect(res.status).to.eq(422)
+                    expect(res.body).to.be.an('object')
+                    expect(res.body).to.have.property('success');
+                    expect(res.body).to.have.property('error');
+                    let { success, error } = res.body;
+                    expect(success).to.eq(false);
+                    expect(error).to.be.an('string');
+                    expect(error).to.eq('Password is wrong');
+                    
+                    // expect(data.token).to.eq(decoded)
+                    done();
+                })
+        })
+
+        it('Should not successfully logged in because email doesn\'t exist', done => {
+            let data = {
+                email: 'test03@mail.com',
+                password: '123456',
+            }
+            chai.request(server)
+                .post('/api/v1/users/login')
+                .set('Content-Type', 'application/json')
+                .send(JSON.stringify(data))
+                .end(function (err, res) {
+                    // console.log(res.body);
+                    expect(res.status).to.eq(422)
+                    expect(res.body).to.be.an('object')
+                    expect(res.body).to.have.property('success');
+                    expect(res.body).to.have.property('error');
+                    let { success, error } = res.body;
+                    expect(success).to.eq(false);
+                    expect(error).to.be.an('string');
+                    expect(error).to.eq('Email doesn\'t exist');
+                    
                     // expect(data.token).to.eq(decoded)
                     done();
                 })
